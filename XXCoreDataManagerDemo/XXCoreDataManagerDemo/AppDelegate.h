9cfd48b67c490c3521167f317f7dd976d3abee4a
xuxueing <330578304@qq.com>
2017-02-05 10:12:25 +0800

    demo

---
+++ XXCoreDataManagerDemo/XXCoreDataManagerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XXCoreDataManagerDemo
+//
+//  Created by 朱小亮 on 2017/2/5.
+//  Copyright © 2017年 朱小亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XXCoreDataManagerDemo/XXCoreDataManagerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XXCoreDataManagerDemo
+//
+//  Created by 朱小亮 on 2017/2/5.
+//  Copyright © 2017年 朱小亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
